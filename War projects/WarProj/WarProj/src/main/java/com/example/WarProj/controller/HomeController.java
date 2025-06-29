1ba0352eccb6bb2b05e7fa80d1befcc43c2d226d
Magesh <mageshvelayudham.3011@gmail.com>
2025-06-29 18:21:18 +0530

    new changed in the home controller

--- War projects/WarProj/WarProj/src/main/java/com/example/WarProj/controller/HomeController.java
+++ War projects/WarProj/WarProj/src/main/java/com/example/WarProj/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
 	@GetMapping("/home")
 	public String home() {
-		return "Hello welcome to home page";
+		return "Hello welcome to home page, deployed by jenkins";
 	}
 }
